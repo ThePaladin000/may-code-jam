ff004a9111393783198a8a99bae613ebde05f4b4
Joshua Gomez <gomezjoshuam@gmail.com>
2025-05-16 19:59:52 -0400

    add logic to close modal using close button or escape key

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("Testing");
+const modal = document.querySelector(".modal");
+const closeButton = document.querySelector(".modal__close-btn");
+const spinButton = document.querySelector(".spin__wheel-arrow");
+
+function modalOpen() {
+  modal.classList.add("modal__open");
+  document.addEventListener("keydown", keydownListener);
+}
+
+function modalClose() {
+  modal.classList.remove("modal__open");
+  document.removeEventListener("keydown", keydownListener);
+}
+
+function keydownListener(event) {
+  if (event.key === "Escape" || event.key === "Esc") {
+    modalClose();
+  }
+}
+
+closeButton.addEventListener("click", modalClose);
+spinButton.addEventListener("click", modalOpen);
